b20b8ebb61c263c93970d0fe66c4776b80f6c84f
taxelrod <taxelrod@git.lsstcorp.org>
2008-02-03 05:07:13 +0000

    Added match multiplicity; Cleaned up a little

--- summarizeRun.py
+++ summarizeRun.py
@@ -7,10 +7,17 @@ Run as summarizeRun.py runId
 """
 import string, sys, os
 
-# set up mysql access
-mySqlHost = 'lsst10.ncsa.uiuc.edu'
-mySqlUser = 'test'
-mySqlPasswd = 'globular.test'
+def execQuery(query):
+
+    # set up mysql access
+    mySqlHost = 'lsst10.ncsa.uiuc.edu'
+    mySqlUser = 'test'
+    mySqlPasswd = 'globular.test'
+
+    mySqlCmd = 'mysql -h %s -u %s -p%s -e \'%s\'' % (mySqlHost, mySqlUser, mySqlPasswd, mySqlQuery)
+
+    print os.popen(mySqlCmd).read()
+    
 
 # get input args
 runId = sys.argv[1]
@@ -22,9 +29,7 @@ print 'Run: %s\n' % (runId)
 
 mySqlQuery = 'use %s; select count(*) Num_Visits from Raw_FPA_Exposure; select count(*) Num_CCD_Visits from Raw_CCD_Exposure; select ROUND(rawFPAExposureId/2) visit, ra, decl, filterId, dateObs, expTime, airmass from Raw_FPA_Exposure;' % (runId)
 
-mySqlCmd = 'mysql -h %s -u %s -p%s -e \'%s\'' % (mySqlHost, mySqlUser, mySqlPasswd, mySqlQuery)
-
-print os.popen(mySqlCmd).read()
+execQuery(mySqlQuery)
 
 # Get number of DIASources matching existing Objects and number that are new
 
@@ -32,9 +37,15 @@ print 'Number of DIASources matching exiting Objects; number that are new\n'
 
 mySqlQuery = 'use %s; select count(*) Num_Matching from DiaSourceToObjectMatches; select count(*) Num_New from NewObjectIdPairs;' % (runId)
 
-mySqlCmd = 'mysql -h %s -u %s -p%s -e \'%s\'' % (mySqlHost, mySqlUser, mySqlPasswd, mySqlQuery)
+execQuery(mySqlQuery)
 
-print os.popen(mySqlCmd).read()
+# Get average match multiplicity
+
+print 'Average and max number of sources matched to each object\n'
+
+mySqlQuery = 'use %s; select AVG(a.nmatch) Avg_match_multiplicity, MAX(a.nmatch) Max_match_multiplicity from (select count(*) nmatch, second from DiaSourceToObjectMatches group by second, visitId) a;' % (runId)
+
+execQuery(mySqlQuery)
 
 # Get number of MopsPreds that were detected
 
@@ -42,9 +53,7 @@ print 'Number of MopsPreds that were detected\n'
 
 mySqlQuery = 'use %s; select count(*) Num_Detected from MopsPredToDiaSourceMatches;' % (runId)
 
-mySqlCmd = 'mysql -h %s -u %s -p%s -e \'%s\'' % (mySqlHost, mySqlUser, mySqlPasswd, mySqlQuery)
-
-print os.popen(mySqlCmd).read()
+execQuery(mySqlQuery)
 
 # Get number of DIASources for each Visit and CCD
 
@@ -52,12 +61,4 @@ print 'Number of DIASources for each Visit and CCD\n'
 
 mySqlQuery = 'use %s; select ROUND(jj.rawFPAExposureId/2) visit, jj.url, count(*) nSources from DIASource s inner join (select f.rawFPAExposureId, c.rawCCDExposureId, c.url from Raw_FPA_Exposure f inner join Raw_CCD_Exposure c on f.rawFPAExposureId=c.rawFPAExposureId) jj on s.ccdExposureId = jj.rawCCDExposureId group by jj.url;;' % (runId)
 
-mySqlCmd = 'mysql -h %s -u %s -p%s -e \'%s\'' % (mySqlHost, mySqlUser, mySqlPasswd, mySqlQuery)
-
-print os.popen(mySqlCmd).read()
-
-
-
-
-
-
+execQuery(mySqlQuery)
